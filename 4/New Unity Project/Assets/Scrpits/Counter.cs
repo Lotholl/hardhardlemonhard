b4114684837013184d9130e8cf62b1cad9dfe18a
S0rago <linehalt10909@yandex.ru>
2020-10-11 23:27:44 +0300

    Button clicks count

---
+++ 4/New Unity Project/Assets/Scrpits/Counter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+
+    //btnId - count
+	private Dictionary<string, int> buttonPresses;
+    
+    //btnId - textFieldName
+    private Dictionary<string, string> fieldToBtn;
+    private TextMeshProUGUI text;
+	void Awake()
+	{
+		buttonPresses = new Dictionary<string, int> ();	
+        fieldToBtn = new Dictionary<string, string> ();		
+	}
+
+    public void AddClick(string btnName)
+    {
+        Debug.Log("Pressed: " + btnName);
+
+        //if false current == 0
+        buttonPresses.TryGetValue(btnName, out var cur);
+        buttonPresses[btnName] = cur + 1;
+    }
+
+    public void GetPresses()
+    {
+        foreach (KeyValuePair<string, int> btnStat in buttonPresses)
+        {
+            UpdateLine(btnStat.Key, btnStat.Value);
+        }
+    }
+
+    private void UpdateLine(string btnName, int count)
+    {
+        if (fieldToBtn.TryGetValue(btnName, out var textName))
+        {
+            Debug.Log(textName);
+            text = GameObject.Find("Canvas/StatMenu/" + textName).GetComponent<TextMeshProUGUI>();
+            text.SetText($"Кнопка {btnName}: {count}");
+        } else {
+            Debug.Log("New button found: " + btnName);
+            fieldToBtn[btnName] = btnName + "Counter";
+            UpdateLine(btnName, count);
+        }
+    }
+}
---
+++ 4/New Unity Project/Assets/Scrpits/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+	public GameObject canvas;
+	private Counter counterScript;
+	public void Awake()
+	{
+		counterScript = canvas.GetComponent<Counter>();
+	}
+	
+	public void ShowHello(Button btn)
+	{
+		// SceneManager.LoadScene("Greeting");
+		Debug.Log("Привет!");
+		OnClick(btn);
+	}
+	
+	public void ShowStats(Button btn)
+	{
+		Debug.Log("Статистика");
+		OnClick(btn);
+		canvas.transform.Find("StatMenu").gameObject.SetActive(true);
+		counterScript.GetPresses();
+		canvas.transform.Find("MainMenu").gameObject.SetActive(false);
+	}
+
+	// public void ReturnToMenu()
+	// {
+	// 	SceneManager.LoadScene("Menu");
+	// }
+	
+	public void Quit(Button btn)
+	{
+		Debug.Log("Выход");
+		Application.Quit();
+		OnClick(btn);
+	}
+
+	public void OnClick(Button btn)
+	{
+		counterScript.AddClick(btn.name);
+	}
+}
