88f72df1f06eea742970854df1021d4f221304de
MorgothS <zaqqaz2136@gmail.com>
2020-10-17 21:56:51 +0300

    My commit

---
+++ 7/New Unity Project/Assets/DecSize.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DecSize : IncSize
+{
+    public void Decrease()
+    {
+        scale = scale - 0.1f;
+        gameObject.transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
---
+++ 7/New Unity Project/Assets/Load.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class Load : Save
+{
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+        public void OnLoadF() // функция чтения
+    {
+        StreamReader sr = new StreamReader(@"savedata.txt"); // открываем файл
+        string textu = sr.ReadLine(); // читаем строку
+        if (textu.Equals("Egypt texture"))
+        {
+            meshRenderer.material.SetTexture("_MainTex", Egypt);
+        }
+        if (textu.Equals("Bricks texture"))
+        {
+            meshRenderer.material.SetTexture("_MainTex", Bricks);
+        }
+
+        scale = float.Parse(sr.ReadLine());
+
+        gameObject.transform.localScale = new Vector3(scale, scale, scale);
+
+
+        sr.Close(); // закрываем файл
+    }
+}
