aa30e9026e6838a48a15c45b3164e1d26ea77bf9
stamacake <30477203+stamacake@users.noreply.github.com>
2020-10-17 23:46:52 +0300

    fix
    fix

--- 1/MyFirstGame/Assets/script/mover.cs
+++ 1/MyFirstGame/Assets/script/mover.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +8,12 @@ public class mover: MonoBehaviour {
   public Vector3 direction;
   public float x = 0;
   public float y = 0;
+  
   public float z = 0;
-  public static int lineVertexCount = 25;
+  public float R = 0;
+  public static int lineVertexCount = 25; // frames
+  public string clipName = "anim1";
+  public float AnimationTime = 1;
   public static float lineLength = 2f;
   public static float lineScaleX = 5f;
   public static float lineScaleY = 5f;
@@ -34,30 +38,32 @@ public class mover: MonoBehaviour {
     return z;
   }
 
-  Keyframe[] keysX = new Keyframe[25];
-  Keyframe[] keysY = new Keyframe[25];
-  Keyframe[] keysZ = new Keyframe[25];
+  Keyframe[] keysX = new Keyframe[lineVertexCount];
+  Keyframe[] keysY = new Keyframe[lineVertexCount];
+  Keyframe[] keysZ = new Keyframe[lineVertexCount];
   AnimationCurve curveX;
   AnimationCurve curveY;
   AnimationCurve curveZ;
   // create a new AnimationClip
   AnimationClip clip;
   Animation anim;
-  
+
   
   // Start is called before the first frame update
   void Start() {
     anim = GetComponent < Animation > ();
     clip = new AnimationClip();
     clip.legacy = true;
+	clip.name = clipName;
+	
     for (int i = lineVertexCount; i-->0;) {
       Debug.Log(Time.time);
       float t = Time.time;
       float angle = angleOffset * i;
       float scale = 2 / (3 - Mathf.Cos(2 * (angle + t * frequency)));
-      keysX[i] = (new Keyframe(i, moveX(angle, scale, t)));
-      keysY[i] = (new Keyframe(i, moveY(angle, scale, t)));
-      keysZ[i] = (new Keyframe(i, moveZ(angle, scale, t)));
+      keysX[i] = (new Keyframe(AnimationTime/lineVertexCount*i, moveX(angle, scale, t)));
+      keysY[i] = (new Keyframe(AnimationTime/lineVertexCount*i, moveY(angle, scale, t)));
+      keysZ[i] = (new Keyframe(AnimationTime/lineVertexCount*i, moveZ(angle, scale, t)));
     }
     curveX = new AnimationCurve(keysX);
     curveY = new AnimationCurve(keysY);
@@ -65,7 +71,7 @@ public class mover: MonoBehaviour {
     clip.SetCurve("", typeof(Transform), "localPosition.x", curveX);
     clip.SetCurve("", typeof(Transform), "localPosition.y", curveY);
     clip.SetCurve("", typeof(Transform), "localPosition.z", curveZ);
-    anim.AddClip(clip, clip.name);
+    anim.AddClip(clip, clipName);
   }
 
   void FixedUpdate() {
@@ -83,13 +89,12 @@ public class mover: MonoBehaviour {
     if (Input.GetKey(KeyCode.RightArrow)) {
       // animate the GameObject
       anim[clip.name].speed = 10f;
-      anim.Play(clip.name);
+      anim.Play(clipName);
     }
 
   }
   
   public void cubeAction(){
-	  anim[clip.name].speed = 10f;
-      anim.Play(clip.name);
+      anim.Play(clipName);
   }
 }
