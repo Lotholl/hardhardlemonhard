ddd35f8b0f20c13c4d21f4200c0f9e4dfc283ad4
GomaJoma <jomaGit@yandex.ru>
2020-10-17 17:00:36 +0300

    Ладно, тильт отменяется, работа готова! (P.s. есть 1 микробаг, мб пофикшу скоро)

--- 5/Car Movements/Assets/Scripts/CarMoves.cs
+++ 5/Car Movements/Assets/Scripts/CarMoves.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class CarMoves : MonoBehaviour
 {
-    public float speedValue = 0f;
-    //private float speedValue = 0f;
-
-    public float maxFrontSpeed = 15f;
-    public float maxRearSpeed = 7f;
-    public float accelerationValue = 1f;
-    //public float inertialBrakingValue = 0.1f;
-    //public float brakingValue = 0.5f;
-    private float inertialBrakingValue = 0f;
-    private float brakingValue = 0f;
-
-    public float rotationSpeed = 45f;
+    public float speedValue = 0f;   // Текущая скорость
+    public float maxFrontSpeed = 10f; // Максимальная скорость при движении вперёд
+    public float maxRearSpeed = 4f; // Максимальная скорость при движении назад
+    public float accelerationValue = 0.25f; // Постоянное ускорение
+    private float inertialBrakingValue = 0f; // Велечина замедления по инерции
+    private float brakingValue = 0f; // Величина замедления при нажатии на "тормоз"
+    public float rotationSpeed = 60f; // Скорость вращения объекта
 
 
     // Направление движения
@@ -26,94 +21,89 @@ public class CarMoves : MonoBehaviour
     private int rotation = -1;
     private const int leftRotation = -1;
     private const int rightRotation = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         inertialBrakingValue = accelerationValue * 0.5f;
-        brakingValue = accelerationValue * 4f;
+        brakingValue = accelerationValue * 0.75f;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Движение вперёд
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && (speedValue == 0) && (direction == rearDirection)) // Смена направления движения
         {
-            if ((speedValue == 0) && (direction == rearDirection))
+            direction *= -1;
+        }
+        if (Input.GetKey(KeyCode.W) && (direction == frontDirection))  // Если мы уже движемся назад, не сможем двигаться вперёд
+        {
+            if (speedValue < maxFrontSpeed)  // Ускорение до максимальной скорости
             {
-                direction *= -1;
+                speedValue += accelerationValue;
             }
-            if (direction == frontDirection)
-            {
-                if (speedValue < maxFrontSpeed)
-                {
-                    speedValue += accelerationValue;
-                }
-                Move(direction);
-            }
+            Move(direction);  // Движение объекта
         }
 
         // Движение назад
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && (speedValue == 0) && (direction == frontDirection)) // Смена направления движения
         {
-            if ((speedValue == 0) && (direction == frontDirection))
-            {
-                direction *= -1;
-            }
-            if (direction == rearDirection)
-            {
-                if (speedValue < maxRearSpeed)
-                {
-                    speedValue += accelerationValue;
-                }
-                Move(direction);
-            }
-
+            direction *= -1;
         }
-        // Движение по инерции
-        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && (speedValue > 0))
+        if (Input.GetKey(KeyCode.S) && (direction == rearDirection))  // Если мы уже движемся назад, не сможем двигаться вперёд
         {
-            speedValue -= inertialBrakingValue;
+            if (speedValue < maxRearSpeed)   // Ускорение до максимальной скорости
+            {
+                speedValue += accelerationValue;
+            }
             Move(direction);
-            //MyLerp();
         }
 
-
+        // Движение по инерции и торможение
+        if (speedValue > 0)
+        {
+            if (Input.GetKey(KeyCode.S) && (direction == frontDirection)) // Если нажали на "тормоз", то торможение будет быстрее, чем по инерции
+            {
+                speedValue -= brakingValue;  // Замедление с "тормозом"
+            }
+            else
+            {
+                speedValue -= inertialBrakingValue;  // Замедление по инерции
+            }
+            Move(direction);
+        }
 
         /////////// Ready ////////////
 
         // Поворот налево
         if (Input.GetKey(KeyCode.A))
         {
-            if (rotation == rightRotation)
+            if (rotation == rightRotation)  // Смена направления поворота
             {
                 rotation *= -1;
             }
-            Rotate(rotation);
+            Rotate(rotation);   // Поворот объекта
         }
 
         // Поворот направо
         if (Input.GetKey(KeyCode.D))
         {
-            if (rotation == leftRotation)
+            if (rotation == leftRotation)   // Смена направления поворота
             {
                 rotation *= -1;
             }
-            Rotate(rotation);
+            Rotate(rotation);   // Поворот объекта
         }
     }
 
-    private void Move(int direction)
+    private void Move(int direction)  // Функция движения объекта
     {
         transform.Translate(Vector3.forward * direction * Time.deltaTime * speedValue);
     }
 
-    private void Rotate(int rotation)
+    private void Rotate(int rotation)  // Функция поворота объекта вокруг своей оси
     {
         transform.Rotate(Vector3.up * rotation * Time.deltaTime * rotationSpeed);
     }
-    //private void MyLerp()
-    //{
-    //    transform.position = Vector3.Lerp(transform.position, Vector3.forward, Time.deltaTime * speedValue);
-    //}
 }
